5b2bd3496509eba545484dd8ae3c9e46c717f055
Anushtha <anushthaniranjan@gmail.com>
2021-05-10 17:41:39 +0530

    sidebar

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,9 +10,33 @@ const $messages = document.querySelector('#messages')
 //templates
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
+const sidebarTemplate = document.querySelector('#sidebar-template').innerHTML
 
 //Options
 const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
+
+// const autoscroll = () => {
+//     // New message element
+//     const $newMessage = $messages.lastElementChild
+
+//     // Height of the new message
+//     const newMessageStyles = getComputedStyle($newMessage)
+//     const newMessageMargin = parseInt(newMessageStyles.marginBottom)
+//     const newMessageHeight = $newMessage.offsetHeight + newMessageMargin
+
+//     // Visible height
+//     const visibleHeight = $messages.offsetHeight
+
+//     // Height of messages container
+//     const containerHeight = $messages.scrollHeight
+
+//     // How far have I scrolled?
+//     const scrollOffset = $messages.scrollTop + visibleHeight
+
+//     if (containerHeight - newMessageHeight <= scrollOffset) {
+//         $messages.scrollTop = $messages.scrollHeight
+//     }
+// }
 
 
 socket.on('message', (message) => {
@@ -33,6 +57,14 @@ socket.on('locationMessage', (message) => {
         createdAt: moment(message.createdAt).format('H:mm a')
     })
     $messages.insertAdjacentHTML('beforeend', html)
+})
+
+socket.on('roomData', ({ room, users }) => {
+    const html = Mustache.render(sidebarTemplate, {
+        room,
+        users
+    })
+    document.querySelector('#sidebar').innerHTML = html
 })
 
 $messageForm.addEventListener('submit', (e) => {
